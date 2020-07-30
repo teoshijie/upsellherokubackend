a7130eb60f7765c16f957f4a0d5f6a5120f8a34d
Shi Jie Teo <teoshijie@gmail.com>
2020-07-30 08:19:47 +0800

    modified users schema

--- models/users.js
+++ models/users.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-        username: { type: String },
-        password: { type: String },
+        username: {
+                type: String,
+                required: true,
+                min: 6,
+                max: 15
+        },
+        password: {
+                type: String,
+                required: true,
+                min: 8,
+                max: 15
+        },
         created_date: { type: Date, default: Date.now },
         shoppingcart: [
                 {
@@ -11,6 +21,31 @@ const userSchema = new mongoose.Schema({
                 }
         ]
 });
+
+UserSchema.pre('save', function (next) {
+        if (!this.isModified('password'))
+                return next();
+        bcrypt.hash(this.password, 10, (err, passwordHash) => {
+                if (err)
+                        return next(err);
+                this.password = passwordHash;
+                next();
+        });
+
+});
+
+UserSchema.methods.comparePassword = function (password, cb) {
+        bcrypt.compare(password, this.password, (err, isMatch) => {
+                if (err)
+                        return cb(err);
+                else {
+                        if (!isMatch)
+                                return cb(null, isMatch);
+                        return cb(null, this)
+                }
+        })
+}
+
 
 const Users = mongoose.model('users', userSchema);
 
