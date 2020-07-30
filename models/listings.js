1096a9d43b800dfe2bf45c7f6996e24baab051f7
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-07-30 09:17:24 +0800

    add seed in db

--- models/listings.js
+++ models/listings.js
@@ -4,9 +4,11 @@ const listingSchema = new mongoose.Schema({
         name: { type: String },
         category: { type: String },
         description: { type: String },
-        image_url: [],
+        image_url: {type: String},
         quantity: { type: Number },
         price: {type: mongoose.Types.Decimal128 },
+        meetup: {type: String},
+        condition: {type: String},
         created_date: { type: Date, default: Date.now },
         last_updated_date: { type: Date, default: Date.now },
         userID: { type: String }
