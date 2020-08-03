242176dd720f15e452517d124ba741268ef1051e
Shi Jie Teo <teoshijie@gmail.com>
2020-08-03 13:00:03 +0800

    added authenticate route

--- controllers/user.js
+++ controllers/user.js
@@ -58,5 +58,10 @@ router.get('/logout', passport.authenticate('jwt', {session: false}), (req, res)
     
 })
 
+userRouter.get('/authenticated',passport.authenticate('jwt',{session : false}),(req,res)=>{
+    const {username} = req.user;
+    res.status(200).json({isAuthenticated : true, user : {username,role}});
+});
+
 
 module.exports = router;
