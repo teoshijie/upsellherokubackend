7e98ea0347651860a954c9204b19d759494dd504
Shi Jie Teo <teoshijie@gmail.com>
2020-08-05 21:56:15 +0800

    edit cors

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 
 
 require('./db');
 
 
 //middlewares 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000' , credentials: true }));
 app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //routes
 require('./routes')(app);
