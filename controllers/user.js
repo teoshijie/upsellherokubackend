44a2da527d2f249666549592a1721b89469f0961
Danny <siewkhee1990@hotmail.com>
2020-08-05 22:49:51 +0800

    small fixes

--- controllers/user.js
+++ controllers/user.js
@@ -8,9 +8,9 @@ const JWT = require('jsonwebtoken');
 
 const signToken = userID => {
     return JWT.sign({
-        iss : "superdanny",
-        sub: userID 
-    }, "superdanny", {expiresIn: "1h"})
+        iss: "superdanny",
+        sub: userID
+    }, "superdanny", { expiresIn: "1h" })
 }
 
 //find all route
@@ -19,6 +19,11 @@ router.get('/', (req, res) => {
         res.json(foundUser);
     });
 });
+
+router.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+})
 
 //find by username by id route 
 router.get('/:userID', (req, res) => {
@@ -33,45 +38,41 @@ router.get('/:userID', (req, res) => {
 
 // User register 
 router.post('/signup', (req, res) => {
-    const {username, password, email, mobile} = req.body
+    const { username, password, email, mobile } = req.body
     console.log(req.body)
-    User.findOne({username}, (err,user) => {
-        if (err){
-            res.status(500).json({message: {msgbody: "Error has occured!", msgError: true}})
-        } else if (user){
-            res.status(500).json({message: {msgbody: "Username is already taken!", msgError: true}})
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            res.status(500).json({ message: { msgbody: "Error has occured!", msgError: true } })
+        } else if (user) {
+            res.status(500).json({ message: { msgbody: "Username is already taken!", msgError: true } })
         } else {
-            const newUser = new User({username, password,email, mobile});
+            const newUser = new User({ username, password, email, mobile });
             console.log(newUser)
-            newUser.save(err=> {
-                if (err){
-                    res.status(500).json({message: {msgbody: "Username is already taken!", msgError: true}})
-                } else{
-                    res.status(201).json({message: {msgbody: "Account successfully create", msgError: false}})
+            newUser.save(err => {
+                if (err) {
+                    res.status(500).json({ message: { msgbody: "Username is already taken!", msgError: true } })
+                } else {
+                    res.status(201).json({ message: { msgbody: "Account successfully create", msgError: false } })
                 }
             })
         }
-    })  
+    })
 })
 
-router.post('/login', passport.authenticate('local', {session: false}), (req, res) => {
-    if(req.isAuthenticated()){
-        const {_id, username} = req.user;
+router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+    if (req.isAuthenticated()) {
+        const { _id, username } = req.user;
         const token = signToken(_id);
-        res.cookie('access_token', token, {httpOnly: true, sameSite: true});
-        res.status(200).json({isAuthenticated: true, user: {username}});
+        res.cookie('access_token', token, { httpOnly: true, sameSite: true });
+        res.status(200).json({ isAuthenticated: true, user: { username } });
     }
 })
 
-router.get('/logout', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.get('/logout', passport.authenticate('jwt', { session: false }), (req, res) => {
     res.clearCookie('access_token');
-    res.json({user:{username: ""}, success: true});
+    res.json({ user: { username: "" }, success: true });
 })
 
-router.get('/authenticated', passport.authenticate('jwt', {session: false}), (req, res) => {
-        const {username,role} = req.user;
-        res.status(200).json({isAuthenticated: true, user: {username,role}});
-    })
-    
+
 
 module.exports = router;
