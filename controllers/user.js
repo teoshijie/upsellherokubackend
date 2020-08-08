31bd222e65afabbd5a71707cb87909761dda788d
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-08-08 11:52:16 +0800

    updated backend as at 8Aug

--- controllers/user.js
+++ controllers/user.js
@@ -22,8 +22,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const { username, role } = req.user;
-    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+    const { username, _id } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, _id } });
 })
 
 router.get('/logout', passport.authenticate('jwt', { session: false }), (req, res) => {
