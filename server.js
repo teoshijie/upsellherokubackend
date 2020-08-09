5a81671138bd7f2879f58868e08fb726a797a044
Shi Jie Teo <teoshijie@gmail.com>
2020-08-09 14:12:26 +0800

    edited server.js

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ require('./db');
 
 //middlewares
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", frontEndUrl);
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accesstoken, ApiAccessToken, Accept, AppID");
-    next();
-});
+app.use(cors({
+    methods: 'GET,POST,PATCH,DELETE,OPTIONS',
+    optionsSuccessStatus: 200,
+    origin: frontEndUrl,
+    allowedHeaders: 'Content-Type, Authorization',
+    exposedHeaders: 'Content-Range, X-Content-Range, Authorization',
+    credentials: true
+  }));
 
 app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json());
