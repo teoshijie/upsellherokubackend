ef35ea678d2bb71b8c038ae91dd86dbc81a6a390
Shi Jie Teo <teoshijie@gmail.com>
2020-08-09 13:56:32 +0800

    edited server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('./db');
 //middlewares 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", frontEndUrl);
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accesstoken, ApiAccessToken, Accept, AppID");
     next();
