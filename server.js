7a4057bbacaaf35802b3288f636eb3d5a63d3b3f
Shi Jie Teo <teoshijie@gmail.com>
2020-08-09 14:07:01 +0800

    edited server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Dependencies 
+//Dependencies
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,17 +10,16 @@ const frontEndUrl = process.env.Front_End_URL || 'http://localhost:3000';
 require('./db');
 
 
-//middlewares 
+//middlewares
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", frontEndUrl);
-    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accesstoken, ApiAccessToken, Accept, AppID");
     next();
 });
 
-app.options('*', cors());
 app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json());
 app.use(cookieParser());
@@ -33,7 +32,7 @@ require('./routes')(app);
 
 
 
-//this will catch any route that doesn't exist 
+//this will catch any route that doesn't exist
 app.get('*', (req, res) => {
     res.status(404).json('Sorry, page not found')
 })
