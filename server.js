a488e73bb33db3c3904509064b8e5d67b48f126c
Shi Jie Teo <teoshijie@gmail.com>
2020-08-09 14:00:35 +0800

    edited server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('./db');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", frontEndUrl);
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", 'true');
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accesstoken, ApiAccessToken, Accept, AppID");
     next();
